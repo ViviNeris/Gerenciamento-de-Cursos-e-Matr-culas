a16a71fe6c2a4d694b4eb6618cfa980cc1bf2e86
Viviane <viviane.neris@gmail.com>
2025-02-18 16:43:24 -0300

    Segundo commit:versão comentada

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
-
+# Listas com os dicionários que são as entidades cursos, alunos e matrículas
 cursos = [
     {
         'curso_id': '01',
@@ -38,7 +38,6 @@ alunos = [
         'aluno_updated_at': '17/02/2025'
     }
 ]
-
 matriculas = [
     {
         'matricula_id': '01',
@@ -53,36 +52,34 @@ matriculas = [
         'enrollment_date': '17/02/2025'
     }
 ]
-
+# Classe cursos que herda 'Resource' que é um recurso da API RESTFull
 class Cursos(Resource):
-    def get(self):
-        return {'cursos': cursos}
+    def get(self): # Método GET dentro da classe 'Cursos'. 
+        return {'cursos': cursos} # Retorna um dicionário com a lista de cursos quando uma solicitação GET é feita.
 
-class Curso(Resource):
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('curso_name')
+class Curso(Resource): # Classe Curso
+    argumentos = reqparse.RequestParser() # Objeto  'RequestParser' para analisar os argumentos
+    argumentos.add_argument('curso_name') # Argumentos
     argumentos.add_argument('curso_description')
     argumentos.add_argument('curso_duration')
     argumentos.add_argument('curso_created_at')
     argumentos.add_argument('curso_updated_at')
 
-    def encontrar_curso(curso_id):
-        for curso in cursos:
-            if curso['curso_id'] == curso_id:
-                return curso
-        return None
+    def encontrar_curso(curso_id): #Função para encontrar o curso
+        for curso in cursos: # Loop que percorre a lista de cursos
+            if curso['curso_id'] == curso_id: # Se id co curso for igual ao fornecido
+                return curso # Retorna o curso
+        return None # Senão, não retorna nada
 
-    def get(self, curso_id):
-        curso = Curso.encontrar_curso(curso_id)
-        if curso:
-            return curso
-        return {'mensagem':'Curso não encontrado.'}, 404 # Not found --> Não encontrado
+    def get(self, curso_id):# Método GET que têm parametros 'self'(ele mesmo) e 'curso_id
+        curso = Curso.encontrar_curso(curso_id) # Chama a função ' encontrar_curso' da classe 'Curso'  
+        if curso: # Se curso for encontrado
+            return curso # Retornar curso
+        return {'mensagem':'Curso não encontrado.'}, 404 # Senão, Not found --> Não encontrado
 
-    def post(self, curso_id):
-        
-        dados = Curso.argumentos.parse_args()
-
-        novo_curso = {
+    def post(self, curso_id):# Método POST que têm parametros 'self'(ele mesmo) e 'curso_id
+        dados = Curso.argumentos.parse_args() # Variável 'dados que analisa e valida os argumentos fornecidos usando o 'parser'
+        novo_curso = { # Dicionário com informações do novo curso
             'curso_id': curso_id,
             'curso_name': dados['curso_name'],
             'curso_description': dados['curso_description'],
@@ -91,14 +88,12 @@ class Curso(Resource):
             'curso_updated_at': dados['curso_updated_at']
         }
 
-        cursos.append(novo_curso)
+        cursos.append(novo_curso) # Adiciona novo curso a lista de cursos
         return novo_curso, 200 # Código de Sucesso
         
-    def put(self, curso_id):
-
-        dados = Curso.argumentos.parse_args()
-
-        novo_curso = {
+    def put(self, curso_id): # Método PUT que têm parametros 'self'(ele mesmo) e 'curso_id
+        dados = Curso.argumentos.parse_args() # Variável 'dados que analisa e valida os argumentos fornecidos usando o 'parser'
+        novo_curso = { # Dicionário com informações do novo curso
             'curso_id': curso_id,
             'curso_name': dados['curso_name'],
             'curso_description': dados['curso_description'],
@@ -107,19 +102,19 @@ class Curso(Resource):
             'curso_updated_at': dados['curso_updated_at']
         }
 
-        curso = Curso.encontrar_curso(curso_id)
-        if curso:
-            curso.update(novo_curso)
-            return novo_curso, 200 # Código de Sucesso
-        cursos.append(novo_curso)
-        return novo_curso, 201 # Código de Created Criado
+        curso = Curso.encontrar_curso(curso_id) # Chama a função ' encontrar_curso' da classe 'Curso'  
+        if curso: # Se curso for encontrado
+            curso.update(novo_curso) # Atualiza o curso existente com as novas informações
+            return novo_curso, 200 # Retorna o curso criado e o Código de Sucesso
+        cursos.append(novo_curso) # Se o curso não for encontrado, adicionamos ele a nova lista de cursos.
+        return novo_curso, 201 # Retorna o curso criado e o Código de Created
 
+    def delete(self, curso_id): # Método DELETE que têm parametros 'self'(ele mesmo) e 'curso_id
+        global cursos # Declarando que a variável 'cursos' é global para que seja possível mudar a lista 'cursos'
+        cursos = [curso for curso in cursos if curso['curso_id'] != curso_id] # Para cada 'curso' na lista 'cursos' e, se curso for diferente de 'curso_id' mantenha-o na lista
+        return{'mensagem':'Curso deletado.'} # Senão, delete o curso
 
-    def delete(self, curso_id):
-        global cursos
-        cursos = [curso for curso in cursos if curso['curso_id'] != curso_id]
-        return{'mensagem':'Curso deletado.'}
-
+# O mesmo processo acontece na classe 'Aluno'
 class Alunos(Resource):
     def get(self):
         return {'alunos': alunos}
@@ -143,9 +138,7 @@ class Aluno(Resource):
             return aluno
         return {'mensagem':'Aluno não encontrado.'}, 404 # Not found --> Não encontrado
 
-
     def post(self, aluno_id):
-        
         dados = Aluno.argumentos.parse_args()
 
         novo_aluno = {
@@ -162,7 +155,6 @@ class Aluno(Resource):
     def put(self, aluno_id):
 
         dados = Aluno.argumentos.parse_args()
-
         novo_aluno = {
             'aluno_id': aluno_id,
             'aluno_name': dados['aluno_name'],
@@ -183,6 +175,7 @@ class Aluno(Resource):
         alunos = [aluno for aluno in alunos if aluno['aluno_id'] != aluno_id]
         return{'mensagem':'Aluno deletado.'}
 
+# O mesmo processo acontece na classe 'Matriculas'
 class Matriculas(Resource):
     def get(self):
         return {'matriculas': matriculas}
@@ -208,7 +201,6 @@ class Matricula(Resource):
     def post(self, matricula_id):
         
         dados = Matricula.argumentos.parse_args()
-
         nova_matricula = {
             'matricula_id': matricula_id,
             'curso_id': dados['curso_id'],
@@ -222,7 +214,6 @@ class Matricula(Resource):
     def put(self, matricula_id):
 
         dados = Matricula.argumentos.parse_args()
-
         nova_matricula = {
             'matricula_id': matricula_id,
             'curso_id': dados['curso_id'],
@@ -242,35 +233,35 @@ class Matricula(Resource):
         matriculas = [matricula for matricula in matriculas if matricula['matricula_id'] != matricula_id]
         return{'mensagem':'Matrícula deletada.'}
 
-class CursoAlunos(Resource):  # Classe para listar alunos de um curso
-    def get(self, curso_id):
-        alunos_matriculados = []
-        for matricula in matriculas:
-            if matricula['curso_id'] == curso_id:
-                aluno = Aluno.encontrar_aluno(matricula['aluno_id'])
-                if aluno:
-                    alunos_matriculados.append(aluno)
-        return {'alunos': alunos_matriculados}
+class CursoAlunos(Resource):  # Classe 'CursoAlunos' para listar alunos de um curso
+    def get(self, curso_id): # Método GET que têm parametros 'self'(ele mesmo) e 'aluno_id'
+        alunos_matriculados = [] # Lista vazia de alunos matriculados
+        for matricula in matriculas: # Loop que percorre a lista 'matriculas'
+            if matricula['curso_id'] == curso_id: # Se o ID do curso na matrícula for igual ao 'curso_id' fornecido
+                aluno = Aluno.encontrar_aluno(matricula['aluno_id']) # Encontra o aluno 'aluno_id' na matricula
+                if aluno: # Se o aluno for encontrado
+                    alunos_matriculados.append(aluno) # Adiciona esse aluno a lista 'alunos_matriculados'
+        return {'alunos': alunos_matriculados} # Retorna um dicionário com a lista de alunos matriculados no curso
 
-class AlunoCursos(Resource):  # Classe para listar cursos de um aluno
-    def get(self, aluno_id):
-        cursos_matriculados = []
-        for matricula in matriculas:
-            if matricula['aluno_id'] == aluno_id:
-                curso = Curso.encontrar_curso(matricula['curso_id'])
-                if curso:
-                    cursos_matriculados.append(curso)
-        return {'cursos': cursos_matriculados}        
+    class AlunoCursos(Resource):  # Classe 'AlunoCursos' para listar cursos de um aluno
+        def get(self, aluno_id): # Método GET que têm parametros 'self'(ele mesmo) e 'aluno_id'
+            cursos_matriculados = [] # Lista vazia de cursos matriculados
+            for matricula in matriculas: # Loop que percorre a lista 'matriculas'
+                if matricula['aluno_id'] == aluno_id: # Se o ID do aluno na matrícula for igual ao 'aluno_id' fornecido
+                    curso = Curso.encontrar_curso(matricula['curso_id']) # Encontra o curso 'curso_id' na matricula
+                    if curso: # Se o curso for encontrado
+                        cursos_matriculados.append(curso) # Adiciona esse curso a lista 'cursos_matriculados'
+            return {'cursos': cursos_matriculados} # Retorna um dicionário com a lista de cursos que o aluno está matriculado  
     
+# Adicionando todas as rotas
 api.add_resource(Cursos, '/cursos')
 api.add_resource(Curso, '/cursos/<string:curso_id>')
 api.add_resource(Alunos, '/alunos')
 api.add_resource(Aluno, '/alunos/<string:aluno_id>')
 api.add_resource(Matriculas, '/matriculas')
 api.add_resource(Matricula, '/matriculas/<string:matricula_id>')
-
-api.add_resource(CursoAlunos, '/cursos/<string:curso_id>/alunos')  # MANTER
-api.add_resource(AlunoCursos, '/alunos/<string:aluno_id>/cursos')  # MANTER
+api.add_resource(CursoAlunos, '/cursos/<string:curso_id>/alunos')  
+api.add_resource(AlunoCursos, '/alunos/<string:aluno_id>/cursos')  
 
 
 if __name__ == '__main__': # Se o nome for o principal (app.py), rode o programa
